d283628221f1d696e83c40b67c1bc6dcbbafb7b4
harshsharma2024 <harshsharma2024@gmail.com>
2024-11-05 16:10:51 +0530

    Add thread pool

--- main.py
+++ main.py
@@ -3,22 +3,21 @@ from utils import main, plot_graph
 
 
 if __name__ == "__main__":
-
-
-    # Create a list of group sizes to test
-    group_sizes = list(range(2, 100, 8))
+    group_sizes = list(range(5, 100, 8))
     group_sizes += list(range(100, 500, 20))
-    group_sizes += list(range(500, 1000, 50)) 
+    group_sizes += list(range(500, 1000, 50))
     group_sizes += list(range(1000, 2050, 100))
 
+    # group_sizes = [5]
 
-    print("groups size: ", group_sizes)
+    print("Group sizes: ", group_sizes)
 
     time_ms = []
     for grp_size in group_sizes:
+        print("Group size: ", grp_size)
         timediff = main(grp_size)
         time_ms.append(timediff)
         print("Time taken for group size ", grp_size, " is ", timediff)
     print(time_ms)
 
-    plot_graph(group_sizes, time_ms) # Plot the graph
+    plot_graph(group_sizes, time_ms)  # Plot the graph
